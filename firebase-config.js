b5de751712d471cb886b843d817d2009c69a0599
PeekoMeeko <62924215+Toonigy@users.noreply.github.com>
2025-03-03 06:25:16 -0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,10 +2,11 @@ const firebaseConfig = {
     apiKey: "AIzaSyDVxcNNmGrFSer05stnh3IT4sIOirPJAxQ",
     authDomain: "pokipoki-world.firebaseapp.com",
     projectId: "pokipoki-world",
-    storageBucket: "pokipoki-world.firebasestorage.app",
+    storageBucket: "pokipoki-world.appspot.com",
     messagingSenderId: "206191017514",
     appId: "1:206191017514:web:0ab28f7788790eec3a4d53",
     measurementId: "G-M9LNQ5M8RR"
 };
 
+// Initialize Firebase
 firebase.initializeApp(firebaseConfig);
